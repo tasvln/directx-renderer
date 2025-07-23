f8426e0ca9d7ec0e2573fc94f857a0ac3f925130
Temitope Adebayo <tasvln@outlook.com>
2025-07-23 03:35:22 -0400

    window creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,84 @@
 #include <windows.h>
 
-int WINAPI WinMain(HINSTANCE hInstance,
-                   HINSTANCE hPrevInstance,
-                   LPSTR lpCmdLine,
-                   int nCmdShow) {
-    MessageBox(nullptr,
-               L"DirectX App Initialized!",
-               L"Welcome",
-               MB_OK | MB_ICONINFORMATION);
+// #define HInstance() GetModuleHandle(nullptr);
+
+INT windowWidth;
+INT windowHeight;
+
+LPCWSTR gAppName = L"DirectX Graphic Renderer";
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    windowWidth = 1440;
+    windowHeight = 768;
+
+    // Window Class
+    WNDCLASSEXW windowClass = {};
+    windowClass.cbSize = sizeof(WNDCLASSEXW);
+    windowClass.style = CS_HREDRAW | CS_VREDRAW;
+    windowClass.cbClsExtra = 0;
+    windowClass.cbWndExtra = 0;
+
+    windowClass.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+    windowClass.hIconSm = LoadIcon(hInstance, IDI_APPLICATION);
+
+    windowClass.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    windowClass.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1);
+
+    windowClass.lpszClassName = L"DirectWindowClass";
+    windowClass.lpszMenuName = nullptr;
+
+    windowClass.hInstance = hInstance;
+
+    windowClass.lpfnWndProc = WndProc;
+
+    RegisterClassEx(&windowClass);
+
+    // Create and Display Window
+
+    // RECT windowRect = { 0, 0, static_cast<LONG>(pSample->GetWidth()), static_cast<LONG>(pSample->GetHeight()) };
+    // AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+    HWND hwnd = CreateWindowEx(
+        0,
+        L"DirectWindowClass", // class name must match windowClass.lpszClassName
+        gAppName,            // window title
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,       // x
+        0,       // y
+        windowWidth,         // width
+        windowHeight,        // height
+        nullptr,             // parent window
+        nullptr,             // menu
+        hInstance,           // instance handle
+        nullptr              // additional application data
+    );
+
+    if (!hwnd) {
+        MessageBox(0, L"Window creation failed!", 0, 0);
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    // Listen for events (Message Loop)
+    MSG msg = { 0 };
+
+    while (msg.message != WM_QUIT) {
+        if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
     return 0;
 }
